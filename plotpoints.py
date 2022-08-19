370ea9e2f18f37a425b3144f19f026ed85fd7784
ASheehan17 <86810117+ASheehan17@users.noreply.github.com>
2022-08-19 16:47:28 -0500

    Add files via upload

--- plotpoints.py
+++ plotpoints.py
@@ -10,24 +10,15 @@ import folium
 import re
 from statistics import mean
 sites = pd.read_csv("sites.csv")
-print(sites.iloc[1,0])
-x = sites.iloc[1,0]
-lat = x.split()[0]
-long = x.split()[1]
-print(lat)
 
-import re
-#lat = '''51°36'9.18"N'''
-deg, minutes, seconds, direction =  re.split('[°\′″]', long)
-
-
-
-#y = (float(deg) + float(minutes)/60 + float(seconds)/(60*60)) * (-1 if direction in ['W', 'S'] else 1)
-#print(y)
+#Jackson Park Coordiantes need adjustment
 jp = sites.iloc[6,:]
 sites = sites.drop(index = 6)
 lats = []
 longs = []
+
+
+#Change format of coordinates
 def to_degree_decimal(lat,long):
     #adjust lat
     deg, minutes, seconds, direction =  re.split('[°\′″]', lat)
@@ -46,14 +37,10 @@ for i in range(0,len(sites.iloc[:,0])):
     long = sites.iloc[i,0]
     long = long.split()[1]
     
-    #print(lat,long)
     to_degree_decimal(lat,long)
     
-print(lats,longs)
-
-print(len(sites.iloc[:,0]), len(lats))
+#Jackson Park Coordinates need adjustment
 jpcoord = jp[0]
-print(jpcoord)
 jplat = jpcoord.split()[0]
 jplong = jpcoord.split()[1]
 jplat = jplat[0:-2]
@@ -67,5 +54,4 @@ longs.append(float(jplong)*-1)
 map = folium.Map(location=[mean(lats), mean(longs)], zoom_start=10, control_scale=True)
 for i in range(0,len(lats)):
     folium.Marker([lats[i],longs[i]], popup=names[i]).add_to(map)
-    #folium.CircleMarker([lats[i],longs[i]]).add_to(map)
 map.save("map.html")
